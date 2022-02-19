a7461ab027cacfd68cf529259b32296cfa63953a
Alma Alvarado <47694944+almaavocado@users.noreply.github.com>
2022-02-18 17:50:54 -0800

    Version 1

---
+++ MortagePersistence-master/app/src/main/java/com/example/mortagepersistencev1/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.mortagepersistencev1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String MA = "MainActivity";
+    public static Mortgage mortgage;
+
+    //@Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        //TO DO SOMETHING GOES HERE
+        mortgage = new Mortgage(this);
+        setContentView(R.layout.activity_main);
+    }
+    public void modifyData(View v) {
+        Intent myIntent = new Intent(this, DataActivity.class);
+        this.startActivity(myIntent);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        updateView();
+    }
+
+    private void updateView() {
+        TextView amountTV = ( TextView ) findViewById( R.id.amount );
+        amountTV.setText(mortgage.getFormattedAmount( ) );
+        TextView yearsTV = ( TextView ) findViewById( R.id.years );
+        yearsTV.setText( "" + mortgage.getYears( ) );
+        TextView interestTV = (TextView) findViewById(R.id.rate);
+        interestTV.setText("" + mortgage.getRate());
+        TextView monthlyPayment = (TextView) findViewById((R.id.monthly_payment));
+        monthlyPayment.setText(mortgage.formattedMonthlyPayment());
+        TextView totalPayment = (TextView) findViewById(R.id.total_payment);
+        totalPayment.setText((mortgage.formattedTotalPayment()));
+    }
+
+
+
+}
